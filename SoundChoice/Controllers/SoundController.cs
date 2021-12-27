c3ac7f96a7f0168c43c83a5418f165e6608e5a20
George Chonin <georgi.chionin@gmail.com>
2021-12-27 14:57:58 +0200

    Added SoundController, UploadViewModel & an Upload page.

---
+++ SoundChoice/Controllers/SoundController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoundChoice.Controllers
+{
+    public class SoundController : Controller
+    {
+        public IActionResult Upload()
+        {
+            return View();
+        }
+    }
+}
---
+++ SoundChoice/Models/ViewModels/UploadViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoundChoice.Models.ViewModels
+{
+    public class UploadViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+        public string? Type { get; set; }
+        public string? Genre { get; set; }
+        public double? BPM { get; set; }
+        [Required]
+        public FormFile File { get; set; }
+
+    }
+}
