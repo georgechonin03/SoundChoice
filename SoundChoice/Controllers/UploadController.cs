5b58f8857e824afd63c3900ea6a5da6e3e6c7539
George Chonin <georgi.chionin@gmail.com>
2022-02-01 13:34:58 +0200

    Added permitted extensions

--- SoundChoice/Controllers/UploadController.cs
+++ SoundChoice/Controllers/UploadController.cs
@@ -6,12 +6,12 @@ namespace SoundChoice.Controllers
 {
     public class UploadController : Controller
     {
-        private IHostingEnvironment Environment;
-        public UploadController(IHostingEnvironment _environment)
+        private IHostingEnvironment _environment;
+        private string[] _permittedExtensions = { ".mp3", ".wav", ".m4a", ".flac", ".wma", ".aac", ".ogg" };
+        public UploadController(IHostingEnvironment environment)
         {
-            Environment = _environment;
+            _environment = environment;
         }
-        private string _dir = @"F:\Uploaded"; 
         public IActionResult Upload()
         {
             return View();
@@ -19,61 +19,29 @@ namespace SoundChoice.Controllers
         [HttpPost]
         public IActionResult Upload(ApplicationFile upload)
         {
-            string path = Path.Combine(this.Environment.WebRootPath, "Uploads");
+            string path = Path.Combine(this._environment.WebRootPath, "Uploads");
             if (!Directory.Exists(path))
             {
                 Directory.CreateDirectory(path);
             }
 
-            /*
-             * Add validation for types of audio files (wav; ogg; mp3; m4a...)
-             */
+            string fileName = Path.GetFileName(upload.File.FileName);
+            var ext = Path.GetExtension(path);
 
-            string fileName = Path.GetFileName($"{upload.Title}.mp3");
+
             using (var fileStream = new FileStream(Path.Combine(path, fileName),
                 FileMode.Create,
                 FileAccess.Write))
             {
-                upload.File.CopyTo(fileStream);
+                if (string.IsNullOrEmpty(ext) || !_permittedExtensions.Contains(ext))
+                {
+                    throw new Exception("The file is invalid. Please try again.");
+                }
+                else
+                    upload.File.CopyTo(fileStream);
             }
             ViewBag.Data = "data:audio/wav;base64," + Convert.ToBase64String(System.IO.File.ReadAllBytes(Path.Combine(path, fileName)));
             return RedirectToAction("Index", "Home");
         }
-
-        /*
-        [HttpPost]
-        public async Task<IActionResult> Upload(IFormFile file)
-        {
-            await UploadFile(file);
-            return View();
-        }
-        public async Task<bool> UploadFile(IFormFile file)
-        {
-            string path = "";
-            bool isCopied = false;
-            try
-            {
-                if(file.Length > 0)
-                {
-                    string fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
-                    path = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"F:\Uploaded"));
-
-                    using (var fileStream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
-                    {
-                        await fileStream.CopyToAsync(fileStream);
-                    }
-                    isCopied = true;
-                }
-                else
-                {
-                    isCopied = false;
-                }
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-            return isCopied;
-        }*/
     }
 }
